fe5027657d3d00f5eee80bb2d8b762fb1ea6ad68
francis <francisalcantara001@gmail.com>
2021-12-02 14:32:14 -0400

    adjust validations

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -6,11 +6,15 @@ namespace Practica.Models
     {
         public int Id { get; set; }
         [Required]
+        [StringLength(50)]
         public string? Nombre { get; set; }
         [Required]
+        [StringLength(200)]
         public string? Descripcion { get; set; }
+        [Display(Name ="Precio en RD")]
         public double Precio { get; set; }
         public bool Activo { get; set; }
+        [Display(Name ="Fecha de alta")]
         public DateTime FechaDeAlta { get; set; }  
     }
 }
